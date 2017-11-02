c2a9d3b50933702bac809f77d7a739cc5a6e1c31
Evelynpa <evelyn.parra.rojas@gmail.com>
2017-11-02 18:03:04 -0300

    se raeliza ejercicio DOM

---
+++ javascript/app.js
@@ -0,0 +1,36 @@
+function add(){
+	/*obtener el valor*/
+	var comments = document.getElementById("comment").value;
+	/*limpiar textarea*/
+	document.getElementById("comment").value = "";
+	var newComments = document.createElement('div');
+	var cont = document.getElementById('cont');
+	var chek = document.createElement('input');
+	chek.type = 'checkbox';
+
+	var pharagraph = document.createElement('p');
+	pharagraph.classList.add('color');
+	var nodoText = document.createTextNode(comments);
+	pharagraph.appendChild(nodoText);
+
+	var heart = document.createElement('i');
+	heart.classList.add('fa','fa-heart','heart');
+	var trash = document.createElement('i');
+	trash.classList.add('fa','fa-trash','trash');
+
+	newComments.appendChild(chek);
+	newComments.appendChild(trash);
+	newComments.appendChild(heart);
+	newComments.appendChild(pharagraph);
+	cont.appendChild(newComments);
+
+	chek.addEventListener('click',function(){
+		pharagraph.classList.toggle('strike-out');
+	})
+	trash.addEventListener('click',function(){
+		cont.removeChild(newComments);
+	})
+	heart.addEventListener('click',function(){
+		heart.classList.toggle('red');
+	})
+}
